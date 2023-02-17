51a0f8e3a56513306b975fc903348405471a05d2
Maria Eduarda <maria.esilva@sptech.school>
2023-02-17 19:25:13 -0300

    [LET-17] Feature: Create the initial structure of the project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Routes from "./routers/router";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Routes />
+      </div>
+    </>
+  );
+}
+
+export default App;
