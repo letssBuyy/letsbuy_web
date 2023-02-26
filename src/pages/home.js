25b5a7f8dc49429020a20fffd662559170d5d482
Maria Eduarda <maria.esilva@sptech.school>
2023-02-26 18:45:51 -0300

    [LET-26] Feature: include navbar in notFound page and home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Title } from '../assets/styles/home'
+import Navbar from "../components/Navbar";
 
 export default function Home() {
     return (
         <>
+        <Navbar type='principal' isAuthenticated={false}/>
             <div>
-                <Title>Ola mundo</Title>
+                <Title>pagina inicial</Title>
             </div>
         </>
     )
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -2,6 +2,7 @@ export const colors = {
     white: '#FFFFFF',
     black: '#000000',
     pink: '#F14866',
+    pinkHover: '#F0395A',
     gray: '#E6E1DD',
     blackGray: '#555555',
 };
