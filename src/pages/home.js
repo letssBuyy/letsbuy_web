35a9e84bce0c02d4644501081789d0d29e716d1f
Maria Eduarda <maria.esilva@sptech.school>
2023-04-09 23:58:39 -0300

    [N/A] Fix:  resolve conflicts

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,8 @@
-import React from "react";
-import { Title } from '../assets/styles/HomeStyle'
 
 function Home() {
     return (
         <>
-            <div>
-                <Title>ola mundo</Title>
-            </div>
+          
         </>
     )
 }
