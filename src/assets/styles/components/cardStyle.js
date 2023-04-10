d344ad70034c3dc0220e6f2a2c0f558fa6ce9c84
Maria Eduarda <maria.esilva@sptech.school>
2023-04-09 22:02:05 -0300

    [LET-32] Feature: Card component and profile screen

---
+++ src/assets/styles/components/cardStyle.js
@@ -0,0 +1,106 @@
+import styled from "styled-components";
+import { fonts } from "../../../utils/fonts";
+import { colors } from "../../../utils/colors";
+
+export const CardContainer = styled.div`
+    width: 200px;
+    height: 297px;
+    display: flex;
+    flex-direction: column;
+    cursor: pointer;
+`;
+
+export const CardProfile = styled.div`
+    width: 100%;
+    height: 216px;
+
+    img {
+        width: 100%;
+        height: 216px;
+        object-fit: cover;
+        border-radius: 10px;
+    }
+`;
+
+export const ContainerDivs = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    height: 75px;
+
+    position: relative;
+`;
+
+export const InfoAdvertise = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 14px;
+    text-align: start;
+    transition: opacity .1s ease-out .05s;
+
+    h1 {
+        font-family: ${fonts.semiBold};
+        font-size: 18px;
+        color: ${colors.pink};
+        margin-bottom: 3px;
+    }
+
+    p {
+        font-family: ${fonts.medium};
+        font-size: 16px;
+        color: ${colors.blackGray};
+        margin-bottom: 6px;
+    }
+
+    span {
+        font-family: ${fonts.regular};
+        font-size: 14px;
+        color: ${colors.blackGray};
+    }
+`;
+
+export const InfoSalle = styled.div`
+    opacity: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    align-items: center;
+    margin-top: 14px;
+    transition: opacity .15s ease-out, .15s ease-out;
+    opacity: 0;
+
+    img {
+        width: 40px;
+        height: 40px;
+        border-radius: 40px;
+        margin-right: 9px;
+    }
+
+    h1 {
+        font-family: ${fonts.semiBold};
+        font-size: 16px;
+        color: ${colors.pink};
+        margin-bottom: 3px;
+    }
+
+    p {
+        font-family: ${fonts.medium};
+        font-size: 16px;
+        color: ${colors.blackGray};
+    }
+
+    >div:last-child img {
+        width: 24px;
+        height: 24px;
+    }
+`;
+
+export const HeartIcon = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    width: 25px;
+    height: 25px;
+`;
--- src/routers/router.js
+++ src/routers/router.js
@@ -8,17 +8,19 @@ import NotFound from '../pages/NotFound';
 import RecoveryEmail  from '../pages/RecoveryEmail';
 import RecoveryPassword  from '../pages/RecoveryPassword';
 import AboutUs  from '../pages/AboutUs';
+import Profile from '../pages/Profile';
 
 export default function Routers() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route  path='/entrar' element={<Login />} />
+                <Route path='/entrar' element={<Login />} />
                 <Route path='/cadastrar' element={<Register />} />
                 <Route path='/recuperar-email' element={<RecoveryEmail />}/>
                 <Route path='/recuperar-senha' element={<RecoveryPassword />}/>
                 <Route path='/sobre-nos' element={<AboutUs />}/>
+                <Route path='/profile' element={<Profile />}/>
                 <Route  path='*' element={<NotFound />} />
             </Routes>
         </Router>
