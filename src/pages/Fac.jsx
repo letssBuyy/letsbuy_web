74c37d66158e58b98d31d257425fca5196eaf70d
Gabriel Maximiano <gabriel.mgomes@sptech.school>
2023-05-04 21:14:15 -0300

    comeco da criação da caixa do faq

--- src/pages/Fac.jsx
+++ src/pages/Fac.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Container, Title, P } from "../assets/styles/AboutUsStyle";
 import Navbar from "../components/Navbar";
-import Dropdown from 'react-bootstrap/Dropdown';
+import { Accordion, AccordionItem } from '@szhsin/react-accordion';
 
 
 export default function Exibir() {
@@ -28,20 +28,29 @@ export default function Exibir() {
                         <P>Sabemos que pode ser um pouco confuso encontrar todas as informações que você precisa sobre a nossa plataforma, por isso criamos essa página para ajudá-lo(a) a encontrar as respostas que procura. A página de FAQ é um lugar onde você pode obter informações úteis e práticas, além de solucionar suas dúvidas e problemas comuns. Nós queremos garantir que você tenha uma experiência incrível na nossa plataforma e estamos sempre à disposição para ajudá-lo(a).</P>
                     </div>
 
-                        <Dropdown>
-                            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                                Dropdown Button
-                            </Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
-
-
                 </Container>
+                    <Accordion allowMultiple>
+                        <AccordionItem header="Como posso anunciar um produto na plataforma?">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </AccordionItem>
+
+                        <AccordionItem header="Quanto tempo meu anúncio fica disponível na plataforma?">
+                            Quisque eget luctus mi, vehicula mollis lorem. Proin fringilla
+                            vel erat quis sodales. Nam ex enim, eleifend venenatis lectus
+                            vitae, accumsan auctor mi.
+                        </AccordionItem>
+
+                        <AccordionItem header="Como posso editar ou excluir meu anúncio?">
+                            Suspendisse massa risus, pretium id interdum in, dictum sit amet
+                            ante. Fusce vulputate purus sed tempus feugiat.
+                        </AccordionItem>
+
+                        <AccordionItem header="É possível negociar o preço do produto com o comprador?">
+                            Suspendisse massa risus, pretium id interdum in, dictum sit amet
+                            ante. Fusce vulputate purus sed tempus feugiat.
+                        </AccordionItem>
+                    </Accordion>
             </div>
         </>
     )
