575e5ff03a03e222e3b0e80f20fa647fdb411331
Gabriel Maximiano <gabriel.mgomes@sptech.school>
2023-05-28 21:12:14 -0300

    criação dash

--- src/assets/styles/DashboardStyle.js
+++ src/assets/styles/DashboardStyle.js
@@ -19,6 +19,7 @@ export const Container = styled.div`
 }
 `
 
+
 export const CaixaKpi = styled.div`
     height:12vh;
     display:flex;
@@ -26,25 +27,53 @@ export const CaixaKpi = styled.div`
     margin-top:5%;
     `
 export const CaixasFilhas = styled.div`
-    border:solid 2px gray;
+    border:solid 2px #ECE8E5;
     border-radius: 15px;
     width:16%;
+    display:flex;
+    align-items: center;
+    
+`
+
+export const ImagensCaixinhas = styled.div`
+   width:35%;
+   align-self: center;
+   padding-left:8%;
+   padding-top:3%;
+`
+
+export const Quantidades= styled.span`
+    font-size:30px;
+    margin-top:10%;
+    align-self: center;
+    font-family:'${fonts.regular}';
+`
+export const Legendas= styled.span`
+    font-size:12px;
+    font-family:'${fonts.regular}';
+
+`
+
+export const Titulos = styled.div`
+    display:grid;
+    height:100%;
 `
 
 export const CaixaGraficos = styled.div`
-    height:50vh;
+    height:55vh;
+    width:100%;
     display:flex;
     justify-content: space-between;
     margin-top:5%;
     `
 export const Graficos = styled.div`
-    border:solid 2px gray;
+    border:solid 2px #ECE8E5;
     border-radius: 15px;
-    width:40%;
+    width:44%;
 `
 
 export const Limitador = styled.div`
     width:90%;
     margin-left:4.8%;
-    
+
     `
