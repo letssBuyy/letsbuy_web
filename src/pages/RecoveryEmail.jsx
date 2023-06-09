ca8cf2d804a4a9087c8e95dbd61a63343ad609fb
Maria Eduarda <maria.esilva@sptech.school>
2023-06-08 21:37:58 -0300

    [LET-99] Feature: connect with backend recovery email and password

--- src/pages/RecoveryEmail.jsx
+++ src/pages/RecoveryEmail.jsx
@@ -1,21 +1,33 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import { ContainerError, InputContainer, Label } from '../assets/styles/components/InputStyle';
 import { Container, SignIn, Title } from '../assets/styles/loginStyle';
 import { Description } from "../assets/styles/recoveryStyle";
 import IconError from '../assets/images/icon-error.svg';
 import Navbar from "../components/Navbar";
 import { validateEmail } from "../utils/strings"
+import axios from "axios";
+import { url } from "../utils/request";
+import { successAlert, errorAlert } from "../utils/alerts";
 
 export default function Recovery() {
     const [showEmailError, setShowEmailError] = useState(false);
     const [email, setEmail] = useState('');
 
-    function enviar(){
-        if (!validateEmail(email)) {
-            setShowEmailError(true)
+    async function enviar() {
+        if (validateEmail(email)) {
+            try {
+                await axios.post(`${url}/emails/trocar-senha`, {
+                    "email": email
+                }).then(() => {
+                    successAlert("Email enviado com sucesso!")
+                }).catch(() => {
+                    errorAlert("Ocorreu um erro ao enviar o email.")
+                })
+            } catch (e) {
+                errorAlert("Ocorreu um erro ao enviar o email.")
+            }
         } else {
-            setShowEmailError(false)
-            alert("ok")
+            setShowEmailError(true)
         }
     }
 
@@ -23,7 +35,7 @@ export default function Recovery() {
         <>
             <Navbar type='basic'
                 isAuthenticated={false}
-                showBackButton={true} 
+                showBackButton={true}
             />
             <div>
                 <Container>
