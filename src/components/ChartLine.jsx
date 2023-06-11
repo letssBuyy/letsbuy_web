468641b5b0cf19b31db257fdef541b524c3d254d
Gabriel Maximiano <gabriel.mgomes@sptech.school>
2023-06-11 18:27:55 -0300

    Ajustes de versão dos Chart e atualização dos texto da tela FAQ

---
+++ src/components/ChartLine.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useRef } from 'react';
+import Chart from 'chart.js';
+
+const LineChart = ({ data }) => {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    if (chartRef && chartRef.current) {
+      const myChart = new Chart(chartRef.current, {
+        type: 'line',
+        data: data,
+        options: {
+          // Configurações de opções do gráfico de linha
+          maintainAspectRatio: true,
+          responsive:true
+        },
+      });
+    }
+  }, [data]);
+
+
+  return <canvas ref={chartRef} />
+};
+
+export default LineChart;
